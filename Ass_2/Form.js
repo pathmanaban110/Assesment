bffa8bf2fe9314a68470294cb775be089a82832e
Pathmanaban V <92936369+pathmanaban110@users.noreply.github.com>
2022-08-18 18:09:05 +0530

    Add files via upload

---
+++ Ass_2/Form.js
@@ -0,0 +1,40 @@
+function show_value(){
+let btn1=document.getElementById('name').value;
+let btn2=document.getElementById('num').value;
+let btn3=document.getElementById('email').value;
+let btn4=document.getElementById('pass').value;
+let btn5=document.getElementById('f').value;
+let btn6=document.getElementById('m').value;
+let btn7=document.getElementById('cntry1').value;
+let btn8=document.getElementById('cntry2').value;
+let btn9=document.getElementById('cntry3').value;
+let btn10=document.getElementById('cntry4').value;
+let btn11=document.getElementById('cntry5').value;
+let btn12=document.getElementById('Tam').value;
+let btn13=document.getElementById('Eng').value;
+let btn14=document.getElementById('kan').value;
+let btn15=document.getElementById('Mal').value;
+let btn16=document.getElementById('txtarea').value;
+
+document.getElementById('show1').innerHTML=btn1;
+document.getElementById('show2').innerHTML=btn2;
+document.getElementById('show3').innerHTML=btn3;
+document.getElementById('show4').innerHTML=btn4;
+document.getElementById('show5').innerHTML=btn5;
+document.getElementById('show6').innerHTML=btn6;
+document.getElementById('show7').innerHTML=btn7;
+document.getElementById('show8').innerHTML=btn8;
+document.getElementById('show9').innerHTML=btn9;
+document.getElementById('show10').innerHTML=btn10;
+document.getElementById('show11').innerHTML=btn11;
+document.getElementById('show12').innerHTML=btn12;
+document.getElementById('show13').innerHTML=btn13;
+document.getElementById('show14').innerHTML=btn14;
+document.getElementById('show15').innerHTML=btn15;
+document.getElementById('show16').innerHTML=btn16;
+document.getElementById('show17').innerHTML=btn17;
+}
+
+
+
+    
